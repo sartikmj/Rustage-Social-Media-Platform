733ff4994851456e19cde09d617d3cd1c482a01b
sartikmj <sartikmj@gmail.com>
2025-07-29 09:37:01 +0530

    created schema

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt" //for hashing passwords
-import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt" //for hashing/encrpyt passwords
+import jwt from 'jsonwebtoken' //token given to user for authorization 
 import User from '../models/User.js'
 
