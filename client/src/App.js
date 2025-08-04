777ecf7b3cc4e3244aed879e17f49579c0bfe688
sartikmj <sartikmj@gmail.com>
2025-08-05 00:56:39 +0530

    frontend installation and setup

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
+import HomePage from './scenes/homePage'
+import LoginPage from './scenes/loginPage'
+import ProfilePage from './scenes/profilePage'
+
+
+const App = () => {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/home' element={<HomePage />} />
+        <Route path='/profile/:userId' element={<ProfilePage />} />
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
